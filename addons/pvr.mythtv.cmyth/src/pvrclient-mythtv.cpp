30906483d559111c404d0dc60cd501d534677218
janbar <jlbarriere68@gmail.com>
2013-03-16 15:32:46 +0100

    [mythtv-cmyth] Recording file: Release connections on close

--- addons/pvr.mythtv.cmyth/src/pvrclient-mythtv.cpp
+++ addons/pvr.mythtv.cmyth/src/pvrclient-mythtv.cpp
@@ -1552,6 +1552,7 @@ void PVRClientMythTV::CloseRecordedStream()
     XBMC->Log(LOG_DEBUG, "%s", __FUNCTION__);
 
   m_file = MythFile();
+  m_pEventHandler->SetRecordingListener("", m_file);
 
   m_pEventHandler->DisablePlayback();
 
